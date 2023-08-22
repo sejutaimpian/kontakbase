8beeee3f5e53bc2730e6648554d9c0db6d036a11
sejutaimpian <derissulistina@gmail.com>
2023-08-23 06:12:33 +0700

    fix: nomor_telepon pada DatabaseSeeder

--- app/Livewire/FormBuatRoom.php
+++ app/Livewire/FormBuatRoom.php
@@ -2,11 +2,34 @@
 
 namespace App\Livewire;
 
+use App\Models\KontakRoom;
+use Livewire\Attributes\Rule;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
 class FormBuatRoom extends Component
 {
+    #[Rule('required|min:3|unique:kontak_room')]
+    public $kode_kontak = '';
+    #[Rule('required|min:3')]
+    public $judul = '';
+    #[Rule('required|min:3')]
+    public $keterangan = '';
+
+    public $kontak_kontak = [];
+    public $kontak = [''];
+
+    public function simpan_room()
+    {
+        $this->validate();
+        // Kontak_room
+        KontakRoom::create([
+            'kode_kontak' => $this->kode_kontak,
+            'judul' => $this->judul,
+            'keterangan' => $this->keterangan
+        ]);
+    }
+
     #[Title('FormPage')]
     public function render()
     {
--- resources/views/livewire/form-buat-room.blade.php
+++ resources/views/livewire/form-buat-room.blade.php
@@ -7,51 +7,74 @@
                 Formulir Room Kontak
             </h2>
         </div>
-        <form class="">
+        <form class="" wire:submit='simpan_room'>
+            @csrf
             <div class="px-4 py-2 border-gray-900 border-y-2">
                 <div class="text-lg uppercase sm:text-2xl">Room Kontak</div>
                 <div class="flex flex-wrap pt-1">
-                    <label for="kode-room" class="w-full text-base sm:text-lg sm:w-3/12">Kode</label>
-                    <input type="text" name="kode-room" id="kode-room" placeholder="Masukan kode room kontak"
-                        class="w-full sm:w-9/12 px-2 py-0.5 focus-within:outline-none focus-within:ring-1 focus-within:ring-pink-400">
+                    <label for="kode-kontak" class="w-full text-base sm:text-lg sm:w-3/12">Kode</label>
+                    <div class="w-full sm:w-9/12">
+                        <input wire:model='kode_kontak' type="text" id="kode-kontak"
+                            placeholder="Masukan kode room kontak"
+                            class="w-full px-2 py-0.5 focus-within:outline-none focus-within:ring-1 focus-within:ring-pink-400">
+                        @error('kode_kontak')
+                        <div class="text-red-500">
+                            {{ $message }}
+                        </div>
+                        @enderror
+                    </div>
                 </div>
                 <div class="flex flex-wrap pt-1">
                     <label for="judul-room" class="w-full text-base sm:text-lg sm:w-3/12">Judul</label>
-                    <input type="text" name="judul-room" id="judul-room" placeholder="Masukan judul room kontak"
-                        class="w-full sm:w-9/12 px-2 py-0.5 focus-within:outline-none focus-within:ring-1 focus-within:ring-pink-400">
+                    <div class="w-full sm:w-9/12">
+                        <input wire:model='judul' type="text" id="judul-room" placeholder="Masukan judul room kontak"
+                            class="w-full px-2 py-0.5 focus-within:outline-none focus-within:ring-1 focus-within:ring-pink-400">
+                        @error('judul')
+                        <div class="text-red-500">
+                            {{ $message }}
+                        </div>
+                        @enderror
+                    </div>
                 </div>
                 <div class="flex flex-wrap pt-1">
                     <label for="keterangan-room" class="w-full text-base sm:text-lg sm:w-3/12">Keterangan</label>
-                    <input type="text" name="keterangan-room" id="keterangan-room"
-                        placeholder="Masukan keterangan room kontak"
-                        class="w-full sm:w-9/12 px-2 py-0.5 focus-within:outline-none focus-within:ring-1 focus-within:ring-pink-400">
+                    <div class="w-full sm:w-9/12">
+                        <input wire:model='keterangan' type="text" id="keterangan-room"
+                            placeholder="Masukan keterangan room kontak"
+                            class="w-full px-2 py-0.5 focus-within:outline-none focus-within:ring-1 focus-within:ring-pink-400">
+                        @error('keterangan')
+                        <div class="text-red-500">
+                            {{ $message }}
+                        </div>
+                        @enderror
+                    </div>
                 </div>
             </div>
-            <div class="px-4 py-2 border-gray-900 border-y-2" x-data="{banyak:1}">
+            <div class="px-4 py-2 border-gray-900 border-y-2">
                 <div class="mb-2 text-lg uppercase sm:text-2xl">Kontak-kontak</div>
-                <template x-for="item in banyak">
-                    <div class="mb-4 border-t-2 border-t-gray-800">
-                        <div class="flex flex-wrap pt-1">
-                            <label for="nama-depan" class="w-full text-base sm:text-lg sm:w-3/12">Nama Depan</label>
-                            <input type="text" name="nama-depan" id="nama-depan" placeholder="Masukan nama depan"
-                                class="w-full sm:w-9/12 px-2 py-0.5 focus-within:outline-none focus-within:ring-1 focus-within:ring-pink-400">
-                        </div>
-                        <div class="flex flex-wrap pt-1">
-                            <label for="nama-belakang" class="w-full text-base sm:text-lg sm:w-3/12">Nama
-                                Belakang</label>
-                            <input type="text" name="nama-belakang" id="nama-belakang"
-                                placeholder="Masukan nama belakang"
-                                class="w-full sm:w-9/12 px-2 py-0.5 focus-within:outline-none focus-within:ring-1 focus-within:ring-pink-400">
-                        </div>
-                        <div class="flex flex-wrap pt-1">
-                            <label for="nomor-telepon" class="w-full text-base sm:text-lg sm:w-3/12">Nomor
-                                Telepon</label>
-                            <input type="number" max="15" name="nomor-telepon" id="nomor-telepon"
-                                placeholder="Masukan 628..."
-                                class="w-full sm:w-9/12 px-2 py-0.5 focus-within:outline-none focus-within:ring-1 focus-within:ring-pink-400">
-                        </div>
+                @foreach ($kontak as $index => $item)
+                <div class="mb-4 border-t-2 border-t-gray-800">
+                    <div class="flex flex-wrap pt-1">
+                        <label for="nama-depan" class="w-full text-base sm:text-lg sm:w-3/12">Nama Depan</label>
+                        <input type="text" name="nama-depan" id="nama-depan" placeholder="Masukan nama depan"
+                            class="w-full sm:w-9/12 px-2 py-0.5 focus-within:outline-none focus-within:ring-1 focus-within:ring-pink-400">
                     </div>
-                </template>
+                    <div class="flex flex-wrap pt-1">
+                        <label for="nama-belakang" class="w-full text-base sm:text-lg sm:w-3/12">Nama
+                            Belakang</label>
+                        <input type="text" name="nama-belakang" id="nama-belakang" placeholder="Masukan nama belakang"
+                            class="w-full sm:w-9/12 px-2 py-0.5 focus-within:outline-none focus-within:ring-1 focus-within:ring-pink-400">
+                    </div>
+                    <div class="flex flex-wrap pt-1">
+                        <label for="nomor-telepon" class="w-full text-base sm:text-lg sm:w-3/12">Nomor
+                            Telepon</label>
+                        <input type="number" max="15" name="nomor-telepon" id="nomor-telepon"
+                            placeholder="Masukan 628..."
+                            class="w-full sm:w-9/12 px-2 py-0.5 focus-within:outline-none focus-within:ring-1 focus-within:ring-pink-400">
+                    </div>
+                </div>
+                @endforeach
+                {{-- Action --}}
                 <div class="flex justify-end gap-2">
                     <div class="flex items-center overflow-hidden text-white rounded-lg">
                         <button type="button" x-on:click="banyak--"
@@ -86,3 +109,7 @@
         </form>
     </div>
 </div>
+
+{{--
+
+--}}
